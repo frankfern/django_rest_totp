beab2f0e5e0d99c8e8a070c5cd04f2d29f30883f
Frank Fernandez <frankfernogando@gmail.com>
2021-03-12 10:55:25 -0500

    adding jwt urls

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include,re_path
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api/', include('accounts.urls')),
+    path('api/v1/token/', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/v1/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
