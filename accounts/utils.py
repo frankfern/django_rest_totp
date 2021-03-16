f00163ad9e954b33f7cc178ce527ddb120ab50dd
Frank Fernandez <frankfernogando@gmail.com>
2021-03-16 01:06:02 -0400

    fisx token serializers

--- accounts/utils.py
+++ accounts/utils.py
@@ -5,6 +5,8 @@ from django_otp.models import Device
 from django_otp import devices_for_user
 from django_otp.plugins.otp_totp.models import TOTPDevice
 
+from django_otp.plugins.otp_static.models import StaticDevice
+
 
 def otp_is_verified(request):
     """
@@ -33,8 +35,14 @@ def get_user_totp_device(self, user, confirmed=None):
     for device in devices:
         if isinstance(device, TOTPDevice):
             return device
-
     return None
+
+
+def get_user_static_device(self, user, confirmed=None):
+    devices = devices_for_user(user, confirmed=confirmed)
+    for device in devices:
+        if isinstance(device, StaticDevice):
+            return device
 
 
 def get_otp_device_id(user, device=None):
